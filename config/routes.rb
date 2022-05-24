7d2a44298f1c046493b3d82f2e3c418ce9197b57
Dreka Njaramba <drekanj@outlook.de>
2022-05-24 12:26:59 +0200

    treehouscontroller

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: "pages#home"
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  root to: "treehouses#index"
+  resources :treehouses, only: %i[index new create show] do
+    resources :bookings, only: %i[create new]
+  end
+  resources :dashboard, only: %i[show]
 
   # Defines the root path route ("/")
   # root "articles#index"
 end
-
-
-# Rails.application.routes.draw do
-#   root to: “treehouses#home”
-#   resources :treehouses, only: %i[index new create show] do
-#     resources :bookings, only: %i[create new]
-#   end
-#   resources :dashboard, only: %i[show]
-# end
