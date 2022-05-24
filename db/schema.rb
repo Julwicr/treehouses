78108d6b0688d2b1b11fe3e6ed795dec7146aaff
Florian Schneider schneider.f@me.com <schneider.f@me.com>
2022-05-24 12:08:32 +0200

    add db:schema

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_23_155555) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_23_175137) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "check_in"
+    t.date "check_out"
+    t.integer "number_of_guests"
+    t.bigint "user_id", null: false
+    t.bigint "treehouse_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["treehouse_id"], name: "index_bookings_on_treehouse_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
+  end
 
   create_table "treehouses", force: :cascade do |t|
     t.string "address"
@@ -40,5 +52,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_23_155555) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "bookings", "treehouses"
+  add_foreign_key "bookings", "users"
   add_foreign_key "treehouses", "users"
 end
