2d3d58d6370e44e03835db7c192c3fa392251abc
Dreka Njaramba <drekanj@outlook.de>
2022-05-25 15:17:35 +0200

    added dashboardcontroller

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -2,5 +2,7 @@ class DashboardsController < ApplicationController
   def index
     @bookings = policy_scope(Booking)
     @user_bookings = Booking.all.where(user_id: current_user)
+    @user_treehouses = Treehouse.all
+    @all_booked_treehouses = Booking.all
   end
 end
