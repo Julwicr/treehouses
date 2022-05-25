31a454ab27f85a062181df443284c38d551cc7f7
Julien Rolland <rollandjulien13013@gmail.com>
2022-05-25 20:30:29 +0200

    add default status pending

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :treehouse
 
   validates :number_of_guests, :check_in, :check_out, presence: true
+  enum status: %i[approved pending rejected], _default: "pending"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_05_25_132609) do
+ActiveRecord::Schema[7.0].define(version: 2022_05_25_142707) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_05_25_132609) do
     t.bigint "treehouse_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status"
     t.index ["treehouse_id"], name: "index_bookings_on_treehouse_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
